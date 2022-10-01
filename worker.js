c657066d2d0846d108ec5e98236a05658335d378
Nathan Clevenger <nateclev@gmail.com>
2022-10-01 07:13:47 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -3,7 +3,7 @@ import { withParams, json } from 'https://pkg.do/itty-router-extras'
 
 const router = Router()
 
-router.get('/:resource?/:id?', withParams, ({url, resource, id}) => json({ helloFrom: url, resource, id}))
+router.get('/:resource?/:id?', withParams, ({url, resource, id}) => json({ url, resource, id}))
 
 export default {
   fetch: router.handle
